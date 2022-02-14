31418825d1bd70d71c363928a33519c2701e7a66
Mariusz112 <maniek11744@gmail.com>
2022-02-14 12:58:34 +0100

    egz

--- src/Z3/Zadanie3.java
+++ src/Z3/Zadanie3.java
@@ -1,7 +1,21 @@
 package Z3;
+import  java.io.File;
+import  java.io.FileFilter;
 
 class Zadanie3 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!3test");
-    }
+   static File[] podkatalogi1(String katalog){
+       return new File(katalog).listFiles(pathname -> pathname.isDirectory());
+   }
+   static File[] podkatalogi2(String katalog){
+       return new File(katalog).listFiles(new FileFilter() {
+           @Override
+           public boolean accept(File pathname) {
+               return pathname.isDirectory();
+           }
+       });
+   }
+   public static void main(String[] args) {
+       for(File f : podkatalogi1(".")) System.out.println((f.getName()));
+       for(File f : podkatalogi2(".")) System.out.println((f.getName()));
+   }
 }
